e36100cc375549e4091d055179cdfb16a103a403
EricB <ericblancas23@gmail.com>
2017-04-12 15:10:17 -0700

    projects commit

---
+++ imageView/DetailViewController.swift
@@ -0,0 +1,51 @@
+//
+//  DetailViewController.swift
+//  imageView
+//
+//  Created by ericblancas on 4/12/17.
+//  Copyright © 2017 ericblancas. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UITableViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    var selectedImage: String?
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let imageToLoad = selectedImage {
+            imageView.image = UIImage(named: imageToLoad)
+        }
+
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        navigationController?.hidesBarsOnTap = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        navigationController?.hidesBarsOnTap = false
+    }
+
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "detail") as? DetailViewController{
+        vc.selectedImage = pictures[indexPath.row]
+            
+        navigationController?.pushViewController(vc, animated: true)
+        }
+    }
+    
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+
+}
